70d0073f6027b487cfce6fc9461c2614796d58dc
Nandapuneedi Shyam <156819070+shyam9493@users.noreply.github.com>
2024-10-28 00:51:59 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'duo-chat-back.vercel.app',
+  baseURL: 'https://duo-chat-back.vercel.app',
 });
 
 export default function Login() {
